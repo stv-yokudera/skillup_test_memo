e677930d93d863031f668954df4a433c31bb1ab6
y.okudera <mail@sample.com>
2017-05-27 09:56:56 +0900

    Initial Commit

---
+++ skillup_test_6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skillup_test_6
+//
+//  Created by OkuderaYuki on 2017/05/27.
+//  Copyright © 2017年 YukiOkudera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
